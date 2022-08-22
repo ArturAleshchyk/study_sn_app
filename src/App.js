47139223a8981b12dc9bf57b1f2eeaf56f92260b
Arthur <aleshchyk.artur@gmail.com>
2022-08-22 18:08:09 +0300

    create Content component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
+import Content from "./components/Content/Content";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header/>
       <Nav/>
-
-      <div className="content">
-        <div>
-          <img src="#" alt=""/>image
-        </div>
-        <div>avatar + description</div>
-        <div>My posts
-          <div>New post</div>
-          <div>
-            <div>Post 1</div>
-            <div>Post 2</div>
-            <div>Post 3</div>
-          </div>
-        </div>
-      </div>
-
+      <Content/>
     </div>
   );
 }
---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="#" alt=""/>image
+      </div>
+      <div>avatar + description</div>
+      <div>My posts
+        <div>New post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+          <div>Post 3</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Content;
